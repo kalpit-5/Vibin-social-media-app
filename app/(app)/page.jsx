ebe6fe18c1e9123ccfd8dbe4f1706688230efe56
kalpit-5 <kalpitparmar55@gmail.com>
2025-06-07 14:21:15 +0530

    Production ready commit

--- app/(app)/page.jsx
+++ app/(app)/page.jsx
@@ -5,8 +5,8 @@ import { QueryClient } from "@tanstack/react-query";
 
 export const metadata = () => {
   return {
-    title: `Socialhop`,
-    description: `New way to feel freedom`,
+    title: `Vibin`,
+    description: `Where moments become memories`,
   };
 };
 
